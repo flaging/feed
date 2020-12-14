f0060d06095ea5f40052bf5701b7f8b5d9cd0cbf
flaging <liyupeng341@163.com>
2020-12-15 00:06:53 +0800

    Update Sync.py

--- sync_bash/Sync.py
+++ sync_bash/Sync.py
@@ -4,34 +4,40 @@ import urllib.request
 import sys
 import os
 import time
-def parse_url(str1,file):
+def parse_url(str1,file, file_name):
   data = feedparser.parse(str1)
   for entry in data.entries:
     url = entry.link
-    print(url)
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-    req=urllib.request.Request(url, headers=headers)
-    try:
-      resp=urllib.request.urlopen(req)
-    except (urllib.HTTPError,e):
-      print('Error code:', e.code)
-    except (urlllib.URLError, e):
-      print( 'We failed to reache a server.')
-      print('Reason:', e.reason)
-    else:
-      data=resp.read().decode('ISO-8859-1')
-      soup = BeautifulSoup(data,'lxml')
-      file.writelines("\n\n### [" +str(soup.title.get_text())+"]("+url+")")
+    file_back_name= '.' + file_name[0:-3]+'.bk'
+    file_back = open(file_back_name,"a")
+    lines = file_back.read().splitlines()
+    if url not in lines
+      file_back.writelines(url)
+      headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+      req=urllib.request.Request(url, headers=headers)
+      try:
+        resp=urllib.request.urlopen(req)
+      except (urllib.HTTPError,e):
+        print('Error code:', e.code)
+      except (urlllib.URLError, e):
+        print( 'We failed to reache a server.')
+        print('Reason:', e.reason)
+      else:
+        data=resp.read().decode('ISO-8859-1')
+        soup = BeautifulSoup(data,'lxml')
+        file.writelines("\n\n### [" +str(soup.title.get_text())+"]("+url+")")
 
 
 def main():
   localtime = time.localtime(time.time())
   file_name = str(localtime.tm_year)+"/"+str(localtime.tm_mon)+str(localtime.tm_mday)+".md"
+  is_exits = os.path.exists(file_name)
   file=open(file_name,"a")
-  file.write("\n# "+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday))
+  if not is_exists:
+    file.write("\n# "+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday))
   url_list = ['http://export.arxiv.org/rss/cs.AR','http://120.53.237.72:1200/weibo/user/6134470959']
   for url in url_list:
-    parse_url(url, file)
+    parse_url(url, file, file_name)
   file.close()
   readme = open("README.md","a")
   readme.writelines("\n\n["+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday)+"]("+file_name+")")
