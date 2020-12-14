6f4ee7d0bf9435adeda0ba1476cd6eab61d1870c
flaging <liyupeng341@163.com>
2020-12-15 00:41:17 +0800

    Update Sync.py

--- sync_bash/Sync.py
+++ sync_bash/Sync.py
@@ -11,7 +11,7 @@ def parse_url(str1,file, file_name):
   lines = file_back.read().splitlines()
   for entry in data.entries:
     url = entry.link
-    if url not in lines:
+    if lines.contains(url):
       file_back.writelines(url+'\n')
       lines.append(url)
       headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
