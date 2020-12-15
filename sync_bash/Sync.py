6f02e69394da3f70c0b0f9777d0a19a9acd7bf6e
flaging <liyupeng341@163.com>
2020-12-16 01:32:20 +0800

    Update Sync.py

--- sync_bash/Sync.py
+++ sync_bash/Sync.py
@@ -4,30 +4,52 @@ import urllib.request
 import sys
 import os
 import time
-def parse_url(str1,file, file_name):
+import sqlite3
+def parse_url(str1,file):
   data = feedparser.parse(str1)
-  file_back_name= file_name[0:-3]+'.bk'
-  file_back = open(file_back_name,"a+")
-  lines = file_back.read().splitlines()
+  if os.path.exists('test.db'):
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+  else:
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+    sql_text_1 = '''CREATE TABLE scores 
+              ( ID NUMBER,
+                URL TEXT);''' 
+    cur.execute(sql_text_1)
+  
   for entry in data.entries:
     url = entry.link
-    if not lines.__contains__(url):
-      file_back.writelines(url+'\n')
-      lines.append(url)
+    exe2= "SELECT * FROM scores WHERE URL == " + '"'+url+'"'
+    print(exe2)
+    result = cur.execute(exe2)
+    count = 0
+    for i in result:
+      print(i)
+      count = count + 1
+    # exe3 = "select * from scores"
+    # result1 = cur.execute(exe3)
+    # for i in result1:
+    #   print(i)
+    print(count)
+    if count == 0:
+      exe1 = 'INSERT INTO scores VALUES(1,"'+url+'")'
+      print(exe1)
+      cur.execute(exe1)
+      conn.commit()
       headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
       req=urllib.request.Request(url, headers=headers)
       try:
         resp=urllib.request.urlopen(req)
-      except (urllib.HTTPError,e):
+      except (urllib.error.HTTPError,e):
         print('Error code:', e.code)
-      except (urlllib.URLError, e):
+      except (urlllib.error.URLError, e):
         print( 'We failed to reache a server.')
         print('Reason:', e.reason)
       else:
         data=resp.read().decode('ISO-8859-1')
         soup = BeautifulSoup(data,'lxml')
         file.writelines("\n\n### [" +str(soup.title.get_text())+"]("+url+")")
-  file_back.close()
 
 
 def main():
@@ -39,13 +61,12 @@ def main():
     file.write("\n# "+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday))
   url_list = open('list.txt','r').read().splitlines()
   for url in url_list:
-    parse_url(url, file, file_name)
+    parse_url(url, file)
   file.close()
   if not is_exists:
     readme = open("README.md","a")
     readme.writelines("\n\n["+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+str(localtime.tm_mday)+"]("+file_name+")")
     readme.close()
   
-
 if __name__ == "__main__":
   main()
